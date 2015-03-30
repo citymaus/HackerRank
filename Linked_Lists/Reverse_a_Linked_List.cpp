06692c24247d8b276275e22e5974b2ec5e8790b9
citymaus <citymaus@users.noreply.github.com>
2015-03-30 01:10:15 -0400

    cleaned up code

--- Linked_Lists/Reverse_a_Linked_List.cpp
+++ Linked_Lists/Reverse_a_Linked_List.cpp
@@ -23,45 +23,23 @@ Node* Reverse(Node *head)
 {
   // Complete this method
     Node* prev_node = NULL;
-    Node* curr_node = head;
     Node* next_node = head->next;
     
     /*cout << "BEFORE:";
     PrintMe(head);
     cout << endl; */
     
-    while (curr_node)
+    while (head)
     {
-        next_node = curr_node->next;
-        curr_node->next = prev_node;
-        
-        /*cout << "prev:";
-        if (prev_node)
-            cout << prev_node->data;
-        else
-            cout << "NULL";
-        cout << " curr:" << curr_node->data << " next:";
-        if (next_node)
-            cout << next_node->data;
-        else 
-            cout << "NULL";
-        cout << endl;*/
-        
-        if (next_node)
-        {
-            Node* temp = curr_node;
-            curr_node = next_node;
-            prev_node = temp;
-        }
-        else
-        {
-            break;    
-        }
+        next_node = head->next;
+        head->next = prev_node;
+		prev_node = head;
+		head = next_node;
     }
     
     /*cout << "AFTER:";
     PrintMe(curr_node);
     cout << endl; */
     
-    return curr_node;
+    return prev_node;
 }
